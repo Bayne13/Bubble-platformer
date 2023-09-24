c0426c96cfd8c0d6e6606333d9eda24ab5b6242f
Bayne13 <Bayneplayz@gmail.com>
2023-09-23 21:17:24 -0400

    started work

---
+++ Bubble-Platformer-Godot-Project/scenes/Level1.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print('level1 ready')
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _input(event):
+	if event.is_action_pressed("return_to_main_menu"):
+		get_tree().change_scene_to_file("res://scenes/Main_Menu.tscn")
